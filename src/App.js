0ea79acd7e73b92bb2a756fa438568822bcaa1b2
Khalidshah12 <kssshah6@gmail.com>
2022-09-08 23:26:12 +0530

    added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with me
+          Learn React
         </a>
       </header>
     </div>
